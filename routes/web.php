0a0af54f4cab8a222cf7460a8ee22bd45db57a48
Ryvel Timtothy Stamber <ryvel.timothy@gmail.com>
2020-10-24 14:44:47 +0700

    navbar dan footer

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('pages.landing');
+});
+Route::get('/welcome', function () {
     return view('welcome');
 });
